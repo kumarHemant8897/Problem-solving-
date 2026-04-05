7b5789395178a943adb09ba0815de2c965da81f4
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-05 20:58:04 +0530

    Time: 27 ms (54.09%), Space: 88.2 MB (67.31%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -19,18 +19,20 @@ public:
 
         int st = 1;
         int end = maxi;
+        int ans=0;
 
         while(st <= end){
             int mid = st + (end - st) / 2;
 
             if(solve(can, mid) >= k){
                 st = mid + 1;
+                ans=mid;
             }
             else{
                 end = mid - 1;
             }
         }
 
-        return end;
+        return ans;
     }
 };
