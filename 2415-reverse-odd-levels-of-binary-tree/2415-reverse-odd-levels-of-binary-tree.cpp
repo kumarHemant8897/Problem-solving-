c9eafe3fe9cc42e80de3ad004ec264e1c45b924a
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-10 15:44:09 +0530

    Time: 0 ms (100%), Space: 77.1 MB (90.05%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -1,49 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+void invertodd(TreeNode* leftroot,TreeNode* rightroot,int level){
+    if(leftroot==NULL || rightroot==NULL){
+        return;
+    }
+    if(level %2 ==1){
+        swap(leftroot->val,rightroot->val);
+    }
+    invertodd(leftroot->left,rightroot->right,level+1);
+    invertodd(leftroot->right,rightroot->left,level+1);
+    
+
+
+
+}
+
+
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if(root == NULL){
+        if(root==NULL){
             return NULL;
         }
+        int level=0;
 
-        queue<TreeNode*> q;
-        q.push(root);
-        int level = 0;
-
-        while(!q.empty()){
-            int n = q.size();
-            vector<TreeNode*> nodes;
-
-            for(int i = 0; i < n; i++){
-                TreeNode* node = q.front();
-                q.pop();
-
-                nodes.push_back(node);
-
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-
-            if(level % 2 == 1){
-                int i=0;
-                int j=nodes.size()-1;
-
-                while(i<j){
-
-                swap(nodes[i]->val,nodes[j]->val);
-                i++;
-                j--;
-                }
-            }
-            level++;
-
-           
-           
-        }
-
-        return root;
+           invertodd(root->left,root->right,1);
+           return root;
+         
+        
     }
 };
