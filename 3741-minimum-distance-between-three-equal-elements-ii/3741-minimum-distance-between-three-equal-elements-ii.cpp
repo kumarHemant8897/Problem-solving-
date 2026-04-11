a789ab44d07ce56bebf70b258a0c18fab71f5eca
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-11 21:27:43 +0530

    Time: 391 ms (55.75%), Space: 343.3 MB (74.5%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        
-        unordered_map<int, vector<int>> pos;  // store indices
-        int ans = INT_MAX;
-
-        for(int i = 0; i < nums.size(); i++){
+        unordered_map<int, vector<int>> pos;
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();i++){
             pos[nums[i]].push_back(i);
+        
 
-            // if we have 3 occurrences
-            if(pos[nums[i]].size() >= 3){
-                int n = pos[nums[i]].size();
+        if(pos[nums[i]].size() >=3 ){
+            int n=pos[nums[i]].size();
 
-                int first = pos[nums[i]][n-3];
-                int third = pos[nums[i]][n-1];
+            int first=pos[nums[i]][n-3];
+            int  third = pos[nums[i]][n-1];
 
-                ans = min(ans, 2 * (third - first));
-            }
+            ans=min(ans, 2 * (third-first));
+        }
         }
 
-        return ans == INT_MAX ? -1 : ans;
+        if(ans==INT_MAX){
+            return -1;
+        }
+        else{
+            return ans;
+        }
+
+        
     }
 };
