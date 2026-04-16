fea68e60393540e5c9c9d35b9ea8f650a1ffd61e
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-16 20:10:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.23%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    int getSum(int n){
-       int sum=0;
-       while(n>0){
+
+  int getsum(int n){
+    int sum=0;
+    while(n>0){
         int d=n%10;
         sum+=d*d;
         n=n/10;
-       }
-       return sum;
     }
+    return sum;
+  }
+  
 
     bool isHappy(int n) {
-        int slow = n;
-        int fast = n;
+        int slow=n;
+        int fast=n;
 
-        do {
-            slow = getSum(slow);
-            fast = getSum(getSum(fast));
-        } while(slow != fast);
+        while(true){
+            fast=getsum(getsum(fast));
+            slow=getsum(slow);
 
-        return slow == 1;
+            if(slow==fast){
+                break;
+            }
+        }
+        return slow==1;
+        
     }
 };
