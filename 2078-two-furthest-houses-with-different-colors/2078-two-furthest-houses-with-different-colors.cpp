86accfd0808283e0e61d6d6868eafafb5bb97738
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-20 00:02:34 +0530

    Time: 0 ms (100%), Space: 12.1 MB (72.74%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,29 +4,15 @@ public:
         int n=colors.size();
         int i=0;
         int j=n-1;
-        int maxdis=0;
 
-        while(j>0){
-            if(colors[j] != colors[0]){
-                maxdis=j;
-                break;
-            }
+        while(i<n && colors[i]==colors[n-1]){
+            i++;
+        }
+        while(j>=0 && colors[j]==colors[0]){
             j--;
         }
 
-        while(i<n){
-            if(colors[i] != colors[n-1]){
-                maxdis=max(maxdis,n-1-i);
-                break;
-            }
-            i++;
-        }
-
-        return maxdis;
-
-      
-
-        
+        return max(j,n-1-i);
         
     }
 };
