7847515d3d31fd8f3bcf0d466b7b5e1d732504bc
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-23 00:06:10 +0530

    Time: 0 ms (100%), Space: 13.8 MB (64.73%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-       
         
-        return !(event1[1] < event2[0] || event2[1] < event1[0]);
+        string start1 = event1[0];
+        string end1   = event1[1];
+        
+        string start2 = event2[0];
+        string end2   = event2[1];
+
+        
+        if (end1 < start2 || end2 < start1) {
+            return false;
+        }
+
+       
+        return true;
     }
 };
