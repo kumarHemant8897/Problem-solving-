9ceae4ef68573daa362e058784172c04131685bd
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-04-27 20:40:16 +0530

    Time: 0 ms (100%), Space: 69.2 MB (19.42%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int n=stones.size();
+        int ans=0;
+
+        if(n==2) return abs(stones[1]-stones[0]);
+        for(int i=2;i<n;i++){
+            ans=max(ans,stones[i] - stones[i-2]);
+
+
+        }
+
+        return ans;
+        
+    }
+};
