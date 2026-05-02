6eac8a4693d485d60d43de35959582c82162a4a5
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-02 21:03:20 +0530

    Time: 2 ms (40.17%), Space: 9.5 MB (25.31%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.cpp
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.cpp
@@ -9,14 +9,14 @@ public:
 
         int n = s.size();
 
-        // try every starting position
+       
         for(int i = 0; i <= n; i++) {
 
-            // check prefix match
+           
             if(i + prefix.size() <= n &&
                s.substr(i, prefix.size()) == prefix) {
 
-                // check suffix AFTER prefix
+                
                 for(int j = i + prefix.size(); j <= n; j++) {
 
                     if(j + suffix.size() <= n &&
