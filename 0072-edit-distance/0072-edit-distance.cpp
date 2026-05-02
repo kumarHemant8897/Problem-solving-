a8bb9aa5360baa9600c30152369733394a9f5021
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-02 10:19:08 +0530

    Time: 9 ms (41%), Space: 13.3 MB (39.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,7 +7,7 @@ public:
 
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        // ✅ Correct base cases
+        
         for(int i = 0; i <= n; i++){
             dp[i][0] = i;
         }
@@ -19,7 +19,7 @@ public:
             for(int j = 1; j <= m; j++){
 
                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = dp[i-1][j-1]; 
+                    dp[i][j] = 0+ dp[i-1][j-1]; 
                 }
                 else{
                     int insertOp = 1 + dp[i][j-1];
