c1db1d7dff12c8046f7248ffc23017c6f55891db
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-04 18:18:28 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,11 +6,12 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<m;j++){
-                swap(mat[i][j],mat[j][i]);
+                swap(mat[i][j] , mat[j][i]);
             }
         }
+
         for(int i=0;i<n;i++){
-            reverse(mat[i].begin(),mat[i].end());
+            reverse(mat[i].begin(), mat[i].end());
         }
         
     }
