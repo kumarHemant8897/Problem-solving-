695fd75375a23f3b67aff9dd35f27018c7f04521
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-09 17:46:45 +0530

    Time: 10 ms (86.03%), Space: 18.4 MB (16.18%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -15,22 +15,22 @@ public:
             int bottom = m - layer - 1;
             int right = n - layer - 1;
 
-            // top row
+            
             for (int j = left; j <= right; j++) {
                 elems.push_back(grid[top][j]);
             }
 
-            // right column
+            
             for (int i = top + 1; i <= bottom - 1; i++) {
                 elems.push_back(grid[i][right]);
             }
 
-            // bottom row
+            
             for (int j = right; j >= left; j--) {
                 elems.push_back(grid[bottom][j]);
             }
 
-            // left column
+            
             for (int i = bottom - 1; i >= top + 1; i--) {
                 elems.push_back(grid[i][left]);
             }
@@ -40,29 +40,29 @@ public:
 
             vector<int> rotated(len);
 
-            // counter-clockwise rotation
+           
             for (int i = 0; i < len; i++) {
                 rotated[i] = elems[(i + rot) % len];
             }
 
             int idx = 0;
 
-            // top row
+            
             for (int j = left; j <= right; j++) {
                 grid[top][j] = rotated[idx++];
             }
 
-            // right column
+            
             for (int i = top + 1; i <= bottom - 1; i++) {
                 grid[i][right] = rotated[idx++];
             }
 
-            // bottom row
+            
             for (int j = right; j >= left; j--) {
                 grid[bottom][j] = rotated[idx++];
             }
 
-            // left column
+            
             for (int i = bottom - 1; i >= top + 1; i--) {
                 grid[i][left] = rotated[idx++];
             }
