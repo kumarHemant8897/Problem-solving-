a94f4704a7cfa910a9d1d36166b5679d104ed63b
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-09 10:50:42 +0530

    Time: 4 ms (63.53%), Space: 18.5 MB (96.46%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+
+    bool dfscheck(int node, int vis[], int pathvis[], vector<int> adj[]) {
+
+        vis[node] = 1;
+        pathvis[node] = 1;
+
+        for(auto it : adj[node]) {
+
+            if(!vis[it]) {
+
+                if(dfscheck(it, vis, pathvis, adj) == true) {
+                    return true;
+                }
+            }
+            else if(pathvis[it]) {
+                return true;
+            }
+        }
+
+        pathvis[node] = 0;
+
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<int> adj[numCourses];
+
+        int n = prerequisites.size();
+
+        
+        for(int i = 0; i < n; i++) {
+
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj[u].push_back(v);
+        }
+
+        int vis[numCourses];
+        int pathvis[numCourses];
+
+        
+        for(int i = 0; i < numCourses; i++) {
+            vis[i] = 0;
+            pathvis[i] = 0;
+        }
+
+        
+        for(int i = 0; i < numCourses; i++) {
+
+            if(!vis[i]) {
+
+                if(dfscheck(i, vis, pathvis, adj) == true) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
