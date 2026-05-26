a2a1f53719b08dfeba6db5178126c89c295bd66c
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-05-26 22:29:52 +0530

    Time: 31 ms (57.85%), Space: 54.3 MB (37.43%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        int mod = 1e9 + 7;
+
+        sort(nums.begin(), nums.end());
+
+        // precompute powers of 2
+        vector<int> power(n);
+        power[0] = 1;
+
+        for(int i = 1; i < n; i++) {
+            power[i] = (power[i - 1] * 2LL) % mod;
+        }
+
+        int left = 0;
+        int right = n - 1;
+
+        int ans = 0;
+
+        while(left <= right) {
+
+            if(nums[left] + nums[right] <= target) {
+
+                ans = (ans + power[right - left]) % mod;
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+};
