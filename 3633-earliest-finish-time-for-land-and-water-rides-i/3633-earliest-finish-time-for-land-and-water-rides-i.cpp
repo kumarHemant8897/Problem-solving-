a5d4aee372d41bd5910909c7c64900abc6e7bb22
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-02 10:39:29 +0530

    Time: 0 ms (100%), Space: 92 MB (12.24%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,29 +1,27 @@
 class Solution {
+    int solve(vector<int>& start1, vector<int>& duration1, vector<int>& start2,
+              vector<int>& duration2) {
+        int finish1 = INT_MAX;
+        for (int i = 0; i < start1.size(); i++) {
+            finish1 = min(finish1, start1[i] + duration1[i]);
+        }
+
+        int finish2 = INT_MAX;
+        for (int i = 0; i < start2.size(); i++) {
+            finish2 = min(finish2, max(start2[i], finish1) + duration2[i]);
+        }
+        return finish2;
+    }
+
 public:
     int earliestFinishTime(vector<int>& landStartTime,
                            vector<int>& landDuration,
                            vector<int>& waterStartTime,
                            vector<int>& waterDuration) {
-        
-        int ans = INT_MAX;
-        int n = landStartTime.size();
-        int m = waterStartTime.size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                // Land -> Water
-                int landFinish = landStartTime[i] + landDuration[i];
-                int finish1 = max(landFinish, waterStartTime[j]) + waterDuration[j];
-
-                // Water -> Land
-                int waterFinish = waterStartTime[j] + waterDuration[j];
-                int finish2 = max(waterFinish, landStartTime[i]) + landDuration[i];
-
-                ans = min(ans, min(finish1, finish2));
-            }
-        }
-
-        return ans;
+        int land_water =
+            solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int water_land =
+            solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return min(land_water, water_land);
     }
 };
