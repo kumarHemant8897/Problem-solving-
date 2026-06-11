e8c8aa5a28076b32b35ede90f8606ee897b894b3
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-11 10:29:27 +0530

    Time: 0 ms (100%), Space: 10.6 MB (10.9%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
 
- int solve(vector<int> &arr , int k){
-    int part=1;
-    int elem=0;
+    int solve(vector<int> &nums,int k){
+        int elem=0;
+        int part=1;
 
-    for(int i=0;i<arr.size();i++){
-        if(arr[i] + elem > k){
-            part++;
-            elem=arr[i];
-           
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] + elem <= k){
+                elem+=nums[i];
+            }
+            else{
+                part++;
+                elem=nums[i];
+            }
         }
-        else{
-       
-        elem+=arr[i];
-        }
-             
+
+        return part;
     }
 
-    return part;
- }
+
 
     int splitArray(vector<int>& nums, int k) {
-       int n=nums.size();
-       int sum=0;
-       int maxi=0;
+        int n=nums.size();
+        int maxi=0;
+        int sum=0;
 
-       for(int i=0;i<n;i++){
-        sum+=nums[i];
-        maxi=max(maxi,nums[i]);
-       }
-       
-       int st=maxi;
-       int end=sum;
-
-       while(st<=end){
-        int mid=st+(end-st)/2;
-
-        if(solve(nums,mid) <= k){
-            end=mid-1;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxi=max(maxi,nums[i]);
         }
-        else{
-            st=mid+1;
+
+        int st=maxi;
+        int end=sum;
+        int ans=0;
+
+        while( st <= end){
+            int mid=st+(end-st)/2;
+
+            if(solve(nums,mid) <= k ){
+                end= mid-1;
+
+            }
+            else{
+               st=mid+1;
+            }
         }
-       }
-       return st;
+
+        return st;
         
     }
 };
