d3d4e337f63589a70551b792bf91fadc19c21eca
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-11 10:15:14 +0530

    Time: 51 ms (46%), Space: 61.8 MB (37.13%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-  bool solve(vector<int> &arr, int dist,int m){
-    int countmag=1,last=arr[0];
 
-    for(int i=1;i<arr.size();i++){
-        if(arr[i] - last >= dist){
-            countmag++;
-            last=arr[i];
+    bool poss(vector<int> &arr,int dist,int magnet){
+        int countmag=1;
+        int last=arr[0];
+
+        for(int i=1;i<arr.size() ; i++){
+            if(arr[i] - last >= dist){
+                countmag++;
+                last=arr[i];
+            }
+
+
+            if(countmag >= magnet) return true;
         }
 
-        if(countmag >= m) return true;
-       
-
+        return false;
     }
-    return false;
-  }
+
+
     int maxDistance(vector<int>& position, int m) {
         int n=position.size();
-        sort(position.begin(),position.end());
+        sort(position.begin() , position.end());
         int st=1;
-        int end=position[n-1]-position[0];
+        int end=position[n-1] - position[0];
         int ans=0;
 
-        while(st<=end){
-            int mid=st+(end-st)/2;
+        while( st <= end){
+            int mid = st + (end - st )/2;
 
-
-            if(solve(position,mid,m)){
+            if(poss(position,mid,m)){
                 st=mid+1;
                 ans=mid;
             }
             else{
-                end=mid-1;
+               end= mid-1;
             }
         }
+
         return ans;
         
     }
