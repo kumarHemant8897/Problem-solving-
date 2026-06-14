5ed344dc975ed69ff1dc9b27b0df82c4f075f449
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-14 11:04:55 +0530

    Time: 100 ms (16.97%), Space: 28.9 MB (25.17%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+
+        if (m < 3 || n < 3) return 0;
+
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+        for (int i = 0; i < m; i++) {
+            pq.push({heightMap[i][0], i, 0});
+            pq.push({heightMap[i][n - 1], i, n - 1});
+            vis[i][0] = vis[i][n - 1] = true;
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            pq.push({heightMap[0][j], 0, j});
+            pq.push({heightMap[m - 1][j], m - 1, j});
+            vis[0][j] = vis[m - 1][j] = true;
+        }
+
+        int water = 0;
+        vector<int> dir = {-1, 0, 1, 0, -1};
+
+        while (!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+
+            int h = cur[0];
+            int x = cur[1];
+            int y = cur[2];
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dir[k];
+                int ny = y + dir[k + 1];
+
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n || vis[nx][ny])
+                    continue;
+
+                vis[nx][ny] = true;
+
+                water += max(0, h - heightMap[nx][ny]);
+
+                pq.push({
+                    max(h, heightMap[nx][ny]),
+                    nx,
+                    ny
+                });
+            }
+        }
+
+        return water;
+    }
+};
