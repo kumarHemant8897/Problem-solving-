08eeeb085394a5303267d05b5e5e7bff414de94e
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-18 10:47:08 +0530

    Time: 6 ms (69.77%), Space: 14.2 MB (19.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-   
-   int solve(string &s1, string &s2){
-        int n = s1.size();
-        int m = s2.size();
+    int solve(int idx1, string &word1, int idx2, string &word2,
+              vector<vector<int>> &dp) {
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        if(idx1 < 0) return idx2 + 1;
+        if(idx2 < 0) return idx1 + 1;
 
-        
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
-        for(int j = 0; j <= m; j++){
-            dp[0][j] = j;
-        }
+        if(dp[idx1][idx2] != -1)
+            return dp[idx1][idx2];
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 0+ dp[i-1][j-1]; 
-                }
-                else{
-                    int insertOp = 1 + dp[i][j-1];
-                    int deleteOp = 1 + dp[i-1][j];
-                    int replaceOp = 1 + dp[i-1][j-1];
-
-                    dp[i][j] = min({insertOp, deleteOp, replaceOp});
-                }
-            }
+        if(word1[idx1] == word2[idx2]) {
+            return dp[idx1][idx2] =
+                solve(idx1 - 1, word1, idx2 - 1, word2, dp);
         }
 
-        return dp[n][m];
-   }
+        int insertOp =
+            1 + solve(idx1, word1, idx2 - 1, word2, dp);
 
-   int minDistance(string word1, string word2) {
-        return solve(word1, word2); 
-   }
+        int deleteOp =
+            1 + solve(idx1 - 1, word1, idx2, word2, dp);
+
+        int replaceOp =
+            1 + solve(idx1 - 1, word1, idx2 - 1, word2, dp);
+
+        return dp[idx1][idx2] =
+            min({insertOp, deleteOp, replaceOp});
+    }
+
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return solve(n - 1, word1, m - 1, word2, dp);
+    }
 };
