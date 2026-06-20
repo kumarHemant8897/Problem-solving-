802afceb3a7d01abe635d3d54038d745d6f506d2
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-20 21:19:56 +0530

    Time: 0 ms (100%), Space: 11.7 MB (76.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,17 +3,17 @@ public:
     int lengthOfLongestSubstring(string s) {
         vector<int> dict(256,-1);
         int n=s.size();
-        int st=0;
+        int st=-1;
         int maxlen=0;
 
         for(int i=0;i<n;i++){
-            if(dict[s[i]]  >= st){
-                st=dict[s[i]] +1 ;
+            if(dict[s[i]]  > st){
+                st=dict[s[i]]  ;
             }
 
             dict[s[i]]=i;
 
-            maxlen=max(maxlen, i - st +1);
+            maxlen=max(maxlen, i - st );
         }
 
         return maxlen;
