66a243452d71ea20a73ce3fde2feaf67723b9df0
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-29 18:42:36 +0530

    Time: 3 ms (62.21%), Space: 19.9 MB (42.93%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-   
-   static bool cmp(vector<int> &a, vector<int> &b){
-       return a[1] > b[1];
+
+   static bool cmp(vector<int> &a,vector<int> &b){
+    return a[1] > b[1];
    }
 
+
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        int n=boxTypes.size();
+        int totalunits=0;
+        sort(boxTypes.begin(),boxTypes.end(),cmp);
 
-        int n = boxTypes.size();
-        sort(boxTypes.begin(), boxTypes.end(), cmp);
+        for(int i=0;i<n;i++){
+            int box=boxTypes[i][0];
+            int units=boxTypes[i][1];
 
-        int totunits = 0;
+            int take=min(box,truckSize);
+            totalunits += take * units;
 
-        for(int i = 0; i < n; i++){
-            int boxes = boxTypes[i][0];   
-            int units = boxTypes[i][1];
-
-            int take = min(boxes, truckSize);
-            totunits += take * units;    
             truckSize -= take;
 
-            if(truckSize == 0) break;    
+
+            if(truckSize == 0) break;
         }
 
-        return totunits;
+        return totalunits;
+        
     }
 };
