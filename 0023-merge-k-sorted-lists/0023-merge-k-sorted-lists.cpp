4dca916ec1760b51f0deb34362b8c26c8f04ac56
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-06-29 09:44:23 +0530

    Time: 0 ms (100%), Space: 19 MB (23.35%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,39 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-   ListNode* convert(vector<int> &ans){
 
-        if(ans.size() == 0) return NULL;
+    ListNode* convert(vector<int> &ans){
+        int n=ans.size();
+        if(n==0) return NULL;
 
-        ListNode* head = new ListNode(ans[0]);
-        ListNode* temp = head;
-
-        
-        for(int i = 1; i < ans.size(); i++){
-            temp->next = new ListNode(ans[i]);
-            temp = temp->next;
+        ListNode* head=new ListNode(ans[0]);
+        ListNode* temp=head;
+        for(int i=1;i<n;i++){
+           temp->next=new ListNode(ans[i]);
+           temp=temp->next;
         }
 
         return head;
-   }
+    }
+
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         vector<int> ans;
+        int n=lists.size();
 
-        for(int i = 0; i < lists.size(); i++){
-            ListNode* temp = lists[i];  
+        for(int i=0;i<n;i++){
+            ListNode* temp=lists[i];
 
             while(temp != NULL){
                 ans.push_back(temp->val);
-                temp = temp->next;
+                temp=temp->next;
             }
         }
 
+        sort(ans.begin(),ans.end());
+        return convert(ans);
         
-        sort(ans.begin(), ans.end());
-
-        
-        ListNode* head = convert(ans);
-
-        return head;
     }
 };
