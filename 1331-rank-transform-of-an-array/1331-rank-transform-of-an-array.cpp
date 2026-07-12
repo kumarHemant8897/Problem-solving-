d35df4602cb25f07be30e48a493afbd0b755c94a
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-07-12 09:19:43 +0530

    Time: 35 ms (88.64%), Space: 45.2 MB (33.78%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -4,26 +4,25 @@ public:
         int n=arr.size();
 
         vector<int> temp=arr;
-
-        sort(temp.begin(),temp.end());
-
         unordered_map<int,int> mp;
-        int rank=1;   
+        sort(temp.begin(),temp.end());
+        int rank=1;
 
         for(int i=0;i<temp.size();i++){
-
             if(mp.find(temp[i]) == mp.end()){
                 mp[temp[i]]=rank;
                 rank++;
             }
-        }  
-
+        }
         vector<int> ans;
-
         for(int i=0;i<n;i++){
             ans.push_back(mp[arr[i]]);
-        } 
+        }
 
-        return ans;  
+        return ans;
+
+
+
+        
     }
 };
