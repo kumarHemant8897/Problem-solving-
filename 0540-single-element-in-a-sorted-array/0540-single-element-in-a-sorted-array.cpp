019bb1635c5d886a3cdd5b1a606409f2653c0d54
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-08-10 08:09:34 +0530

    Time: 0 ms (100%), Space: 26.1 MB (69.81%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,20 +5,22 @@ public:
         int st=0;
         int end=n-1;
 
-        while(st<end){
-            int mid=st+(end-st)/2;
+        while(st < end){
+            int mid= st + (end - st ) /2;
 
-            if(mid%2==1){
+
+            if(mid % 2 == 1){
                 mid--;
             }
 
-            if(nums[mid]==nums[mid+1]){
+            if(nums[mid] == nums[mid+1]){
                 st=mid+2;
             }
             else{
                 end=mid-1;
             }
         }
+
         return nums[st];
         
     }
