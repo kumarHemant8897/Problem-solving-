f8c0de457b277e2916b65fbfa3d75a3c3f5f11da
Hament kumar <167906549+kumarHemant8897@users.noreply.github.com>
2026-08-24 09:38:33 +0530

    Time: 477 ms (10.59%), Space: 225.3 MB (15.68%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int solve(int idx, int buy,int cap,vector<int> &prices,vector<vector<vector<int>>> &dp){
-        int n=prices.size();
 
-        if(idx == n) return 0;
-        if(cap == 0) return 0;
+    int solve(int idx,int buy,int cap,vector<int>& prices, vector<vector<vector<int>>> &dp){
+        int n=prices.size();
+        if(idx==n || cap==0){
+            return 0;
+        }
 
         if(dp[idx][buy][cap] != -1){
             return dp[idx][buy][cap];
         }
 
         if(buy){
-            dp[idx][buy][cap]= max(-prices[idx] + solve(idx+1,0,cap,prices,dp) , 0 + solve(idx+1,1,cap,prices,dp) );
+            dp[idx][buy][cap]=max(-prices[idx] + solve(idx+1,0,cap,prices,dp)  , 0 + solve(idx+1,1,cap,prices,dp));
         }
         else{
-            dp[idx][buy][cap]=max(prices[idx]  + solve(idx+1,1,cap-1,prices,dp) , 0 + solve(idx+1,0,cap,prices,dp));
+            dp[idx][buy][cap]=max(prices[idx]+solve(idx+1,1,cap-1,prices,dp) , 0 + solve(idx+1,0,cap,prices,dp));
         }
 
         return dp[idx][buy][cap];
-
-      
     }
+
 
 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int> ( 3,-1)));
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
 
         return solve(0,1,2,prices,dp);
         
